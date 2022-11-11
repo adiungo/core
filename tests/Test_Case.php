7f41f1840e68936d8eae0ab34587f6e67576e185
Alex Standiford <a@alexstandiford.com>
2022-11-11 13:10:11 -0600

    Issue/8 - Set Up PHPUnit (#17)
    * Adds test dependencies.
    
    * Adds Unit test directory
    
    * Sets up PHPUnit configs and yml
    
    * Set up bootstrap file.
    
    * Implement mockery supported base test class
    
    * Adds test that will confirm CI runs before merge
    
    * Adds test that will confirm CI runs before merge
    
    * PHPCS Fix

---
+++ tests/Test_Case.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Adiungo\Core\Tests;
+
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use PHPUnit\Framework\TestCase;
+
+class Test_Case extends TestCase
+{
+
+    use MockeryPHPUnitIntegration;
+}
---
+++ tests/Unit/Valdiate_CI_Test.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Adiungo\Core\Tests\Unit;
+
+
+use Adiungo\Core\Tests\Test_Case;
+
+class Valdiate_CI_Test extends Test_Case
+{
+
+    /**
+     * Validates that the tests run.
+     *
+     * @return void
+     */
+    public function test_tests_run(): void
+    {
+        $this->assertTrue(true);
+    }
+}
