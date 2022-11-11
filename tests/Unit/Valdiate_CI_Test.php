47e809edbf11383580789fcafe66e6734a6543eb
Alex Standiford <a@alexstandiford.com>
2022-11-11 13:05:20 -0600

    Adds test that will confirm CI runs before merge

---
+++ tests/Unit/Valdiate_CI_Test.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Adiungo\Core\Tests\Unit;
+
+
+use Adiungo\Core\Tests\Test_Case;
+
+class Valdiate_CI_Test extends Test_Case
+{
+
+    public function test_tests_run()
+    {
+        $this->assertTrue(true);
+    }
+}
