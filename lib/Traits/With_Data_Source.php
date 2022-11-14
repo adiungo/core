5b23b14e748d6d2b9c11a80f0faadaf746845a9d
Alex Standiford <a@alexstandiford.com>
2022-11-13 19:47:00 -0600

    Sets up with data source class

---
+++ lib/Traits/With_Data_Source.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Adiungo\Core\Traits;
+
+
+namespace Adiungo\Core\Traits;
+
+use Adiungo\Core\Interfaces\Data_Source;
+use Adiungo\Core\Interfaces\Has_Data_Source;
+
+/**
+ * @see Has_Data_Source
+ */
+trait With_Data_Source
+{
+
+    protected Data_Source $data_source;
+
+    public function get_data_source(): Data_Source
+    {
+        return $this->data_source;
+    }
+
+    public function set_data_source(Data_Source $data_source): static
+    {
+        $this->data_source = $data_source;
+
+        return $this;
+    }
+
+}
