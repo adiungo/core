5a8296cb4a7ffc0579710d75822d14d4fae93ca0
Alex Standiford <a@alexstandiford.com>
2022-11-13 18:57:03 -0600

    Introduces content model tests.

---
+++ tests/Unit/Abstracts/Content_Model_Test.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Adiungo\Core\Tests\Unit\Abstracts;
+
+use Adiungo\Core\Tests\Test_Case;
+
+class Content_Model_Test extends Test_Case
+{
+
+    /**
+     * @covers \Adiungo\Core\Abstracts\Content_Model::save
+     *
+     * @return void
+     */
+    public function test_can_save(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @covers \Adiungo\Core\Abstracts\Content_Model::delete
+     *
+     * @return void
+     */
+    public function test_can_delete(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+}
