5801d76e2ee23cc377d3fd6ea180c8e2fe19ab77
Alex Standiford <a@alexstandiford.com>
2022-11-14 06:39:15 -0600

    Builds content model provider class, and tests (#42)
    * Builds content model provider class, and tests
    
    * PHP CS Fix
    
    Signed-off-by: Alex Standiford <a@alexstandiford.com>
    
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

---
+++ lib/Events/Providers/Content_Model_Provider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Adiungo\Core\Events\Providers;
+
+use Adiungo\Core\Abstracts\Content_Model;
+use Underpin\Interfaces\Data_Provider;
+
+class Content_Model_Provider implements Data_Provider
+{
+    /**
+     * Constructor.
+     *
+     * @param Content_Model $content_model
+     */
+    public function __construct(protected Content_Model $content_model)
+    {
+
+    }
+
+    /**
+     * Gets the content model provided by this class.
+     *
+     * @return Content_Model
+     */
+    public function get_model(): Content_Model
+    {
+        return $this->content_model;
+    }
+}
