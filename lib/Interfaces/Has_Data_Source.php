5a037d651fc9e093075d0bece743c1b98221c4fb
Alex Standiford <a@alexstandiford.com>
2022-11-13 19:46:24 -0600

    Introduces the has data source interface

---
+++ lib/Interfaces/Has_Data_Source.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Adiungo\Core\Interfaces;
+
+
+namespace Adiungo\Core\Interfaces;
+
+use Adiungo\Core\Traits\With_Data_Source;
+
+/**
+ * @see With_Data_Source
+ */
+interface Has_Data_Source
+{
+    /**
+     * Gets the data source
+     * @return Data_Source
+     */
+    public function get_data_source(): Data_Source;
+
+    /**
+     * Sets the data source
+     * @param Data_Source $data_source
+     * @return $this
+     */
+    public function set_data_source(Data_Source $data_source): static;
+}
