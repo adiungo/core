2a16f6a6389c353dad6bc61a6b54c215eea51e2b
Alex Standiford <a@alexstandiford.com>
2022-11-14 08:17:49 -0600

    Sets up Content Model methods and tests
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

--- lib/Abstracts/Content_Model.php
+++ lib/Abstracts/Content_Model.php
@@ -3,11 +3,34 @@
 namespace Adiungo\Core\Abstracts;
 
 
+use Adiungo\Core\Events\Content_Model_Event;
+use Adiungo\Core\Events\Providers\Content_Model_Provider;
 use Underpin\Interfaces\Identifiable;
 use Underpin\Interfaces\Model;
 
 abstract class Content_Model implements Model, Identifiable
 {
+
+    /**
+     * Gets the content model event.
+     *
+     * @return Content_Model_Event
+     */
+    protected function get_content_model_event(): Content_Model_Event
+    {
+        return Content_Model_Event::instance();
+    }
+
+    /**
+     * Creates a content model provider, and provides the current instance.
+     *
+     * @return Content_Model_Provider
+     */
+    protected function get_content_model_provider(): Content_Model_Provider
+    {
+        return new Content_Model_Provider($this);
+    }
+
     /**
      * Saves this model.
      *
@@ -15,7 +38,7 @@ abstract class Content_Model implements Model, Identifiable
      */
     public function save(): static
     {
-        //TODO: IMPLEMENT THIS METHOD.
+        $this->get_content_model_event()->broadcast('save', $this->get_content_model_provider());
         return $this;
     }
 
@@ -26,7 +49,7 @@ abstract class Content_Model implements Model, Identifiable
      */
     public function delete(): static
     {
-        //TODO: IMPLEMENT THIS METHOD.
+        $this->get_content_model_event()->broadcast('delete', $this->get_content_model_provider());
         return $this;
     }
 }
--- tests/Unit/Abstracts/Content_Model_Test.php
+++ tests/Unit/Abstracts/Content_Model_Test.php
@@ -2,10 +2,17 @@
 
 namespace Adiungo\Core\Tests\Unit\Abstracts;
 
+use Adiungo\Core\Abstracts\Content_Model;
+use Adiungo\Core\Events\Content_Model_Event;
+use Adiungo\Core\Events\Providers\Content_Model_Provider;
 use Adiungo\Core\Tests\Test_Case;
+use Adiungo\Core\Tests\Traits\With_Inaccessible_Methods;
+use Mockery;
+use ReflectionException;
 
 class Content_Model_Test extends Test_Case
 {
+    use With_Inaccessible_Methods;
 
     /**
      * @covers \Adiungo\Core\Abstracts\Content_Model::save
@@ -14,7 +21,13 @@ class Content_Model_Test extends Test_Case
      */
     public function test_can_save(): void
     {
-        $this->markTestIncomplete();
+        $model = Mockery::mock(Content_Model::class)->shouldAllowMockingProtectedMethods()->makePartial();
+        $provider = Mockery::mock(Content_Model_Provider::class);
+
+        $model->allows('get_content_model_provider')->andReturn($provider);
+        $model->expects('get_content_model_event->broadcast')->with('save', $provider);
+
+        $model->save();
     }
 
     /**
@@ -24,7 +37,45 @@ class Content_Model_Test extends Test_Case
      */
     public function test_can_delete(): void
     {
-        $this->markTestIncomplete();
+        $model = Mockery::mock(Content_Model::class)->shouldAllowMockingProtectedMethods()->makePartial();
+        $provider = Mockery::mock(Content_Model_Provider::class);
+
+        $model->allows('get_content_model_provider')->andReturn($provider);
+        $model->expects('get_content_model_event->broadcast')->with('delete', $provider);
+
+        $model->delete();
     }
 
+    /**
+     * @covers \Adiungo\Core\Abstracts\Content_Model::get_content_model_event
+     *
+     * @throws ReflectionException
+     */
+    public function test_can_get_content_model_event(): void
+    {
+        $instance = Mockery::mock(Content_Model::class)->makePartial();
+        $another_instance = Mockery::mock(Content_Model::class)->makePartial();
+
+        $event = $this->call_inaccessible_method($instance, 'get_content_model_event');
+        $again = $this->call_inaccessible_method($another_instance, 'get_content_model_event');
+        $this->assertInstanceOf(Content_Model_Event::class, $event);
+
+        // Validate that both models fetch the singleton instance.
+        $this->assertSame($again, $event);
+    }
+
+    /**
+     * @covers \Adiungo\Core\Abstracts\Content_Model::get_content_model_provider
+     *
+     * @throws ReflectionException
+     */
+    public function test_can_get_content_model_provider(): void
+    {
+        $instance = Mockery::mock(Content_Model::class);
+
+        /** @var Content_Model_Provider $result */
+        $result = $this->call_inaccessible_method($instance, 'get_content_model_provider');
+
+        $this->assertSame($result->get_model(), $instance);
+    }
 }
