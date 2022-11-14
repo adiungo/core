6393220f068043a6485c6ce48fe02416195e7ba5
Alex Standiford <a@alexstandiford.com>
2022-11-14 06:51:09 -0600

    Issue/25 (#43)
    * Adds Index Strategy class
    
    Signed-off-by: Alex Standiford <a@alexstandiford.com>
    
    * Adds has/with index strategy classes
    
    Signed-off-by: Alex Standiford <a@alexstandiford.com>
    
    * Adds doc block detailing code coverage of with data source
    
    Signed-off-by: Alex Standiford <a@alexstandiford.com>
    
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

---
+++ lib/Interfaces/Has_Index_Strategy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Adiungo\Core\Interfaces;
+
+
+use Adiungo\Core\Factories\Index_Strategy;
+
+interface Has_Index_Strategy
+{
+    /**
+     * Gets the index strategy.
+     *
+     * @return Index_Strategy
+     */
+    public function get_index_strategy(): Index_Strategy;
+
+    /**
+     * Sets the index strategy.
+     *
+     * @param Index_Strategy $index_strategy
+     * @return $this
+     */
+    public function set_index_strategy(Index_Strategy $index_strategy): static;
+}
