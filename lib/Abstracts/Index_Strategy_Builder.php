b2d6180b057274de38916f95a32127f90f55281f
Alex Standiford <a@alexstandiford.com>
2022-11-14 20:37:39 -0600

    Makes get_model public
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

--- lib/Abstracts/Index_Strategy_Builder.php
+++ lib/Abstracts/Index_Strategy_Builder.php
@@ -10,5 +10,5 @@ abstract class Index_Strategy_Builder implements Has_Index_Strategy
 {
     use With_Index_Strategy;
 
-    abstract protected function get_model(): string;
+    abstract public function get_model(): string;
 }
