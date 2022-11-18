cfc6f7bb1e76ca621d1280473ac22b25df4b3eb8
Alex Standiford <a@alexstandiford.com>
2022-11-17 18:47:20 -0600

    Sets up published date implementers.
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

---
+++ tests/Unit/Traits/With_Published_Date_Test.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Adiungo\Core\Tests\Unit\Traits;
+
+
+use Adiungo\Core\Tests\Test_Case;
+use Adiungo\Core\Tests\Traits\With_Simple_Setter_Getter_Tests;
+use Adiungo\Core\Traits\With_Published_Date;
+use DateTime;
+use Generator;
+use Mockery;
+
+/**
+ * @covers \Adiungo\Core\Traits\With_Published_Date::set_published_date
+ * @covers \Adiungo\Core\Traits\With_Published_Date::get_published_date
+ */
+class With_Published_Date_Test extends Test_Case
+{
+    use With_Simple_Setter_Getter_Tests;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->instance = Mockery::mock(With_Published_Date::class);
+        $this->instance->shouldAllowMockingProtectedMethods()->makePartial();
+    }
+
+    protected function get_setters_and_getters(): Generator
+    {
+        yield 'published_date' => ['set_published_date', 'get_published_date', Mockery::mock(DateTime::class)];
+    }
+}
