1ac9b6b0b94c9d2b5db3ce1c479398d06cdf7950
Alex Standiford <a@alexstandiford.com>
2022-11-17 18:37:02 -0600

    Sets up location implementers, including the trait and interface.
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

---
+++ lib/Interfaces/Has_Location.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Adiungo\Core\Interfaces;
+
+use Adiungo\Core\Factories\Location;
+
+interface Has_Location
+{
+
+    /**
+     * Gets the location
+     *
+     * @return Location
+     */
+    public function get_location(): Location;
+
+    /**
+     * Sets the location
+     *
+     * @param Location $location The location to set.
+     * @return $this
+     */
+    public function set_location(Location $location): static;
+}
