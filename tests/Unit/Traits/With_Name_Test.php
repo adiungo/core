48e62d0422c01394d78a3a76641165c60a2ccaa7
Alex Standiford <a@alexstandiford.com>
2022-11-17 19:17:39 -0600

    Sets up name implementers. (#72)
    Signed-off-by: Alex Standiford <a@alexstandiford.com>
    
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

---
+++ tests/Unit/Traits/With_Name_Test.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Adiungo\Core\Tests\Unit\Traits;
+
+
+use Adiungo\Core\Tests\Test_Case;
+use Adiungo\Core\Tests\Traits\With_Simple_Setter_Getter_Tests;
+use Adiungo\Core\Traits\With_Name;
+use Generator;
+use Mockery;
+
+/**
+ * @covers \Adiungo\Core\Traits\With_Name::set_name
+ * @covers \Adiungo\Core\Traits\With_Name::get_name
+ */
+class With_Name_Test extends Test_Case
+{
+    use With_Simple_Setter_Getter_Tests;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->instance = Mockery::mock(With_Name::class);
+        $this->instance->shouldAllowMockingProtectedMethods()->makePartial();
+    }
+
+    protected function get_setters_and_getters(): Generator
+    {
+        yield 'name' => ['set_name', 'get_name', 'foo'];
+    }
+}
