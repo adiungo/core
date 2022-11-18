814bcab51214ef664cbfb70f857f6db38c018f13
Alex Standiford <a@alexstandiford.com>
2022-11-17 18:56:51 -0600

    Sets up updated date implementers.
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

---
+++ lib/Traits/With_Updated_Date.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Adiungo\Core\Traits;
+
+
+namespace Adiungo\Core\Traits;
+
+use DateTime;
+
+trait With_Updated_Date
+{
+
+    protected DateTime $updated_date;
+
+    /**
+     * Gets the updated date
+     *
+     * @return DateTime
+     */
+    public function get_updated_date(): DateTime
+    {
+        return $this->updated_date;
+    }
+
+    /**
+     * Sets the updated date
+     *
+     * @param DateTime $updated_date The updated_date to set.
+     * @return $this
+     */
+    public function set_updated_date(DateTime $updated_date): static
+    {
+        $this->updated_date = $updated_date;
+
+        return $this;
+    }
+
+}
---
+++ tests/Unit/Traits/With_Updated_Date_Test.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Adiungo\Core\Tests\Unit\Traits;
+
+
+use Adiungo\Core\Tests\Test_Case;
+use Adiungo\Core\Tests\Traits\With_Simple_Setter_Getter_Tests;
+use Adiungo\Core\Traits\With_Updated_Date;
+use DateTime;
+use Generator;
+use Mockery;
+
+/**
+ * @covers \Adiungo\Core\Traits\With_Updated_Date::set_updated_date
+ * @covers \Adiungo\Core\Traits\With_Updated_Date::get_updated_date
+ */
+class With_Updated_Date_Test extends Test_Case
+{
+    use With_Simple_Setter_Getter_Tests;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->instance = Mockery::mock(With_Updated_Date::class);
+        $this->instance->shouldAllowMockingProtectedMethods()->makePartial();
+    }
+
+    protected function get_setters_and_getters(): Generator
+    {
+        yield 'updated_date' => ['set_updated_date', 'get_updated_date', Mockery::mock(DateTime::class)];
+    }
+}
