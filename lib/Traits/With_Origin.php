c713a31da464326877cdb508b1c46838f1e4fe8d
Alex Standiford <a@alexstandiford.com>
2022-11-17 18:42:29 -0600

    Sets up origin implementers.
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

---
+++ lib/Traits/With_Origin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Adiungo\Core\Traits;
+
+
+namespace Adiungo\Core\Traits;
+
+use Underpin\Factories\Url;
+
+trait With_Origin
+{
+
+    protected Url $origin;
+
+    /**
+     * Gets the origin
+     *
+     * @return Url
+     */
+    public function get_origin(): Url
+    {
+        return $this->origin;
+    }
+
+    /**
+     * Sets the origin
+     *
+     * @param Url $origin The origin to set.
+     * @return $this
+     */
+    public function set_origin(Url $origin): static
+    {
+        $this->origin = $origin;
+
+        return $this;
+    }
+
+}
