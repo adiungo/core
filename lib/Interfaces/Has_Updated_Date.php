f2088cb3dd8fb2417679af6b11919204e9cff43c
Alex Standiford <a@alexstandiford.com>
2022-11-17 18:57:45 -0600

    Sets up updated date implementers. (#70)
    Signed-off-by: Alex Standiford <a@alexstandiford.com>
    
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

---
+++ lib/Interfaces/Has_Updated_Date.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Adiungo\Core\Interfaces;
+
+use DateTime;
+
+interface Has_Updated_Date
+{
+
+    /**
+     * Gets the updated date
+     *
+     * @return DateTime
+     */
+    public function get_updated_date(): DateTime;
+
+    /**
+     * Sets the updated date
+     *
+     * @param DateTime $updated_date The updated_date to set.
+     * @return $this
+     */
+    public function set_updated_date(DateTime $updated_date): static;
+}
