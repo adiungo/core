917496ce0f4540da77b44d7149a5cc5419288405
Alex Standiford <a@alexstandiford.com>
2022-12-09 19:54:54 -0600

    Introduces the page based batch builder.
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

---
+++ lib/Abstracts/Page_Based_Batch_Builder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Adiungo\Core\Abstracts;
+
+
+use Adiungo\Core\Interfaces\Has_Page;
+use Adiungo\Core\Interfaces\Has_Paginated_Request;
+use Underpin\Traits\With_Request;
+
+abstract class Page_Based_Batch_Builder implements Has_Page, Has_Paginated_Request
+{
+    use With_Request;
+
+    /**
+     * Gets the request for the next page.
+     * @return $this
+     */
+    public function get_next(): static
+    {
+        return $this->set_page($this->get_page() + 1);
+    }
+}
