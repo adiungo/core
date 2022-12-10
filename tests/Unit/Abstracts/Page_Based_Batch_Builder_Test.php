a9c917b498a8d9b85c713ae6bdaa37b1178ba61a
Alex Standiford <a@alexstandiford.com>
2022-12-09 19:57:47 -0600

    Introduces the page based batch builder. (#131)
    Signed-off-by: Alex Standiford <a@alexstandiford.com>
    
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

---
+++ tests/Unit/Abstracts/Page_Based_Batch_Builder_Test.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Adiungo\Core\Tests\Unit\Abstracts;
+
+
+use Adiungo\Core\Abstracts\Page_Based_Batch_Builder;
+use Adiungo\Tests\Test_Case;
+use Mockery;
+
+class Page_Based_Batch_Builder_Test extends Test_Case
+{
+    public function test_can_get_next(): void
+    {
+        $instance = Mockery::mock(Page_Based_Batch_Builder::class)->makePartial();
+
+        $instance->allows('get_page')->andReturn(2);
+        $instance->expects('set_page')->with(3)->andReturn($instance);
+
+        $this->assertSame($instance->get_next(), $instance);
+    }
+}
