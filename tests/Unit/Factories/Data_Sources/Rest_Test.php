9eafe67821f64690a58c78cab8a0610aed07fd6e
Alex Standiford <a@alexstandiford.com>
2022-12-11 07:39:05 -0600

    Implements Rest::get_item
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

--- tests/Unit/Factories/Data_Sources/Rest_Test.php
+++ tests/Unit/Factories/Data_Sources/Rest_Test.php
@@ -3,7 +3,19 @@
 namespace Adiungo\Core\Tests\Unit\Factories\Data_Sources;
 
 
+use Adiungo\Core\Abstracts\Content_Model;
+use Adiungo\Core\Abstracts\Int_Id_Based_Request_Builder;
+use Adiungo\Core\Factories\Data_Sources\Rest;
 use Adiungo\Tests\Test_Case;
+use JsonException;
+use Mockery;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Underpin\Exceptions\Operation_Failed;
+use Underpin\Factories\Request;
 
 class Rest_Test extends Test_Case
 {
@@ -47,10 +59,33 @@ class Rest_Test extends Test_Case
     /**
      * @covers \Adiungo\Core\Factories\Data_Sources\Rest::get_item
      * @return void
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws Operation_Failed
+     * @throws JsonException
      */
     public function test_can_get_item(): void
     {
-        $this->markTestIncomplete();
+        $instance = Mockery::mock(Rest::class)->shouldAllowMockingProtectedMethods()->makePartial();
+        $request = Mockery::mock(Request::class);
+        $response = Mockery::mock(ResponseInterface::class);
+        $builder = Mockery::mock(Int_Id_Based_Request_Builder::class);
+        $expected = Mockery::mock(Content_Model::class);
+
+        $builder->expects('set_id')->once()->andReturn($builder);
+        $builder->expects('get_request')->once()->andReturn($request);
+
+        $response->expects('getContent')->once()->andReturn('{"foo": "bar"}');
+
+        $instance->expects('get_single_request_builder')->once()->andReturn($builder);
+        $instance->expects('make_request')->with($request)->once()->andReturn($response);
+        $instance->expects('get_data_source_adapter->convert_to_model')->once()->andReturn($expected);
+
+        $this->assertSame($instance->get_item(123), $expected);
+        // Run twice to confirm the data is cached and does not run more than once.
+        $this->assertSame($instance->get_item(123), $expected);
     }
 
 }
