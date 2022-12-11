aa3f7fa81d2e4123e54a14e06e2f114ac9b04155
Alex Standiford <a@alexstandiford.com>
2022-12-10 22:50:17 -0600

    Issue 95 - Implement Rest class with stubs
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

---
+++ tests/Unit/Factories/Data_Sources/Rest_Test.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Adiungo\Core\Tests\Unit\Factories\Data_Sources;
+
+
+use Adiungo\Tests\Test_Case;
+
+class Rest_Test extends Test_Case
+{
+
+    /**
+     * @covers \Adiungo\Core\Factories\Data_Sources\Rest::make_request
+     * @return void
+     */
+    public function test_can_make_request(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @covers \Adiungo\Core\Factories\Data_Sources\Rest::get_data
+     * @return void
+     */
+    public function test_can_get_data(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @covers \Adiungo\Core\Factories\Data_Sources\Rest::has_more
+     * @return void
+     */
+    public function test_has_more(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @covers \Adiungo\Core\Factories\Data_Sources\Rest::get_next
+     * @return void
+     */
+    public function test_can_get_next(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @covers \Adiungo\Core\Factories\Data_Sources\Rest::get_item
+     * @return void
+     */
+    public function test_can_get_item(): void
+    {
+        $this->markTestIncomplete();
+    }
+
+}
