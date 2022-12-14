dd5f67541f7635e7c9430fbacbd358e1cbda946d
Alex Standiford <a@alexstandiford.com>
2022-12-13 20:02:13 -0600

    Implements Rest::get_data
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

--- lib/Factories/Data_Sources/Rest.php
+++ lib/Factories/Data_Sources/Rest.php
@@ -7,11 +7,13 @@ use Adiungo\Core\Abstracts\Content_Model;
 use Adiungo\Core\Collections\Content_Model_Collection;
 use Adiungo\Core\Interfaces\Data_Source;
 use Adiungo\Core\Interfaces\Has_Batch_Request_Builder;
+use Adiungo\Core\Interfaces\Has_Batch_Response_Adapter;
 use Adiungo\Core\Interfaces\Has_Content_Model_Instance;
 use Adiungo\Core\Interfaces\Has_Data_Source_Adapter;
 use Adiungo\Core\Interfaces\Has_Has_More_Strategy;
 use Adiungo\Core\Interfaces\Has_Single_Request_Builder;
 use Adiungo\Core\Traits\With_Batch_Request_Builder;
+use Adiungo\Core\Traits\With_Batch_Response_Adapter;
 use Adiungo\Core\Traits\With_Content_Model_Instance;
 use Adiungo\Core\Traits\With_Data_Source_Adapter;
 use Adiungo\Core\Traits\With_Has_More_Strategy;
@@ -28,10 +30,11 @@ use Underpin\Factories\Request;
 use Underpin\Helpers\Array_Helper;
 use Underpin\Traits\With_Object_Cache;
 
-class Rest implements Data_Source, Has_Content_Model_Instance, Has_Data_Source_Adapter, Has_Has_More_Strategy, Has_Batch_Request_Builder, Has_Single_Request_Builder
+class Rest implements Data_Source, Has_Content_Model_Instance, Has_Data_Source_Adapter, Has_Batch_Response_Adapter, Has_Has_More_Strategy, Has_Batch_Request_Builder, Has_Single_Request_Builder
 {
     use With_Content_Model_Instance;
     use With_Data_Source_Adapter;
+    use With_Batch_Response_Adapter;
     use With_Object_Cache;
     use With_Has_More_Strategy;
     use With_Single_Request_Builder;
@@ -39,8 +42,14 @@ class Rest implements Data_Source, Has_Content_Model_Instance, Has_Data_Source_A
 
     public function get_data(): Content_Model_Collection
     {
-        // TODO: Implement get_data() method.
-        return new Content_Model_Collection();
+        return $this->load_from_cache('get_data', function () {
+            $request = $this->get_batch_request_builder()->get_request();
+            $response = $this->get_batch_response_adapter()->set_response($this->make_request($request))->to_array();
+
+            return (new Content_Model_Collection())->seed(Array_Helper::each($response, function (array $data) {
+                return $this->get_data_source_adapter()->convert_to_model($data);
+            }));
+        });
     }
 
     public function has_more(): bool
--- tests/Unit/Factories/Data_Sources/Rest_Test.php
+++ tests/Unit/Factories/Data_Sources/Rest_Test.php
@@ -3,6 +3,7 @@
 namespace Adiungo\Core\Tests\Unit\Factories\Data_Sources;
 
 
+use Adiungo\Core\Abstracts\Batch_Response_Adapter;
 use Adiungo\Core\Abstracts\Content_Model;
 use Adiungo\Core\Abstracts\Has_More_Strategy;
 use Adiungo\Core\Abstracts\Int_Id_Based_Request_Builder;
@@ -33,7 +34,28 @@ class Rest_Test extends Test_Case
      */
     public function test_can_make_request(): void
     {
-        $this->markTestIncomplete();
+        $instance = Mockery::mock(Rest::class)->makePartial()->shouldAllowMockingProtectedMethods();
+        $request = Mockery::mock(Request::class);
+        $response = Mockery::mock(ResponseInterface::class);
+        $adapter = Mockery::mock(Batch_Response_Adapter::class);
+        $expected = ['foo' => 'foo', 'bar' => 'bar'];
+
+        $instance->expects('get_batch_request_builder->get_request')->andReturn($request);
+        $instance->expects('make_request')->with($request)->andReturn($response);
+        $instance->expects('get_batch_response_adapter')->andReturn($adapter);
+        $instance->allows('get_data_source_adapter->convert_to_model')->andReturnUsing(function (array $data) {
+            $result = Mockery::mock(Content_Model::class);
+            $result->allows('get_id')->andReturn($data[0]);
+
+            return $result;
+        });
+
+        $adapter->expects('set_response')->with($response);
+        $adapter->expects('to_array')->andReturn([['foo'], ['bar']]);
+
+        $items = $instance->get_data()->pluck('id');
+
+        $this->assertEquals($expected, $items);
     }
 
     /**
