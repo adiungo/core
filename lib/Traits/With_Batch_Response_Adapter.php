cb03c43d4be90967f16d3c94f710883cc5b797c9
Alex Standiford <a@alexstandiford.com>
2022-12-13 20:06:20 -0600

    Issue/141 - Implements Rest::make_request (#142)
    * Introduces batch response adapter and tests.
    
    I realized that I need a way to convert the rest response and get the array
    
    Signed-off-by: Alex Standiford <a@alexstandiford.com>
    
    * Implements Rest::get_data
    
    Signed-off-by: Alex Standiford <a@alexstandiford.com>
    
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

---
+++ lib/Traits/With_Batch_Response_Adapter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Adiungo\Core\Traits;
+
+
+use Adiungo\Core\Abstracts\Batch_Response_Adapter;
+
+trait With_Batch_Response_Adapter
+{
+    /**
+     * @var Batch_Response_Adapter
+     */
+    protected Batch_Response_Adapter $batch_response_adapter;
+
+    /**
+     * Get the collection that this class can set.
+     *
+     * @return Batch_Response_Adapter
+     */
+    public function get_batch_response_adapter(): Batch_Response_Adapter
+    {
+        return $this->batch_response_adapter;
+    }
+
+    /**
+     * Sets the collection that this class can set.
+     *
+     * @param Batch_Response_Adapter $batch_response_adapter
+     * @return static
+     */
+    public function set_batch_response_adapter(Batch_Response_Adapter $batch_response_adapter): static
+    {
+        $this->batch_response_adapter = $batch_response_adapter;
+
+        return $this;
+    }
+}
---
+++ tests/Unit/Traits/With_Batch_Response_Adapter_Test.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Adiungo\Core\Tests\Unit\Traits;
+
+
+use Adiungo\Core\Abstracts\Batch_Response_Adapter;
+use Adiungo\Core\Traits\With_Batch_Response_Adapter;
+use Adiungo\Tests\Test_Case;
+use Adiungo\Tests\Traits\With_Simple_Setter_Getter_Tests;
+use Generator;
+use Mockery;
+
+/**
+ * @covers \Adiungo\Core\Traits\With_Batch_Response_Adapter::set_data_source
+ * @covers \Adiungo\Core\Traits\With_Batch_Response_Adapter::get_data_source
+ */
+class With_Batch_Response_Adapter_Test extends Test_Case
+{
+    use With_Simple_Setter_Getter_Tests;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->instance = Mockery::mock(With_Batch_Response_Adapter::class);
+        $this->instance->shouldAllowMockingProtectedMethods()->makePartial();
+    }
+
+    protected function get_setters_and_getters(): Generator
+    {
+        yield 'data_source' => ['set_batch_response_adapter', 'get_batch_response_adapter', Mockery::mock(Batch_Response_Adapter::class)];
+    }
+}
