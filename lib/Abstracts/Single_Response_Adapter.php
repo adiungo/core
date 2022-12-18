387ab83c83f80d3eafdaac6119acb1eb8b6da55d
Alex Standiford <a@alexstandiford.com>
2022-12-18 09:47:59 -0600

    Updates single response adapter to convert to array instead.
    Signed-off-by: Alex Standiford <a@alexstandiford.com>

--- lib/Abstracts/Single_Response_Adapter.php
+++ lib/Abstracts/Single_Response_Adapter.php
@@ -4,9 +4,9 @@ namespace Adiungo\Core\Abstracts;
 
 use Adiungo\Core\Interfaces\Has_Response;
 use Adiungo\Core\Traits\With_Response;
-use Underpin\Interfaces\Can_Convert_To_Instance;
+use Underpin\Interfaces\Can_Convert_To_Array;
 
-abstract class Single_Response_Adapter implements Can_Convert_To_Instance, Has_Response
+abstract class Single_Response_Adapter implements Can_Convert_To_Array, Has_Response
 {
     use With_Response;
 }
