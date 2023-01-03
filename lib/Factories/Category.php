3fbfea6a393387778cb8424e14e2504ca17423b9
Alex Standiford <alex@standiford.us>
2023-01-03 06:54:43 -0600

    Category extends content model

--- lib/Factories/Category.php
+++ lib/Factories/Category.php
@@ -2,12 +2,13 @@
 
 namespace Adiungo\Core\Factories;
 
+use Adiungo\Core\Abstracts\Content_Model;
 use Adiungo\Core\Interfaces\Has_Name;
 use Adiungo\Core\Traits\With_Name;
 use Underpin\Interfaces\Identifiable_String;
 use Underpin\Traits\With_String_Identity;
 
-class Category implements Identifiable_String, Has_Name
+class Category extends Content_Model implements Identifiable_String, Has_Name
 {
     use With_String_Identity;
     use With_Name;
