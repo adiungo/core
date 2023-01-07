3bcb3eb657eba9b43a5c6935c94120b27fd06bd7
Alex Standiford <alex@standiford.us>
2023-01-07 12:19:10 -0600

    Issue/186 - Updates Image class to support descriptions

--- lib/Factories/Attachments/Image.php
+++ lib/Factories/Attachments/Image.php
@@ -3,11 +3,14 @@
 namespace Adiungo\Core\Factories\Attachments;
 
 use Adiungo\Core\Abstracts\Attachment;
+use Adiungo\Core\Interfaces\Has_Description;
+use Adiungo\Core\Traits\With_Description;
 use Adiungo\Core\Traits\With_Origin;
 use Underpin\Traits\With_String_Identity;
 
-class Image extends Attachment
+class Image extends Attachment implements Has_Description
 {
     use With_String_Identity;
     use With_Origin;
+    use With_Description;
 }
